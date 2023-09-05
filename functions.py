f8a561c51a04a26da76557102382545a1e45f5b6
Alejandro Sanchez <33365377+Al3x-s@users.noreply.github.com>
2023-09-05 16:05:37 -0500

    finished 293 AB#293

--- functions.py
+++ functions.py
@@ -4,3 +4,30 @@ def print_description_slowly(description, delay=0.02):
     for char in description:         
         print(char, end='', flush=True)         
         time.sleep(delay)
+
+
+def start_menu():
+    print("1.New Game")
+    print("2.Quit")
+    player_choice = input("Enter your choice '1 or 2'> ")
+    if player_choice == "1":
+        start_game()
+    elif player_choice == "2":
+        print("Goodbye!")
+        exit()
+    else:
+        print("invalid answer, try again")
+        start_menu()
+def start_game():
+    player_input = ''
+
+    while player_input != 'quit':
+        # This is where game logic would go
+        print("this is where game logic would go")
+        player_input = input("placeholder to stop infinite loop").lower()
+        if player_input == "quit":
+            print("Goodbye!")
+            exit()
+
+#def play_game(user_start):
+#    return 1
